b4e4740dfbcfd3389bd490d7424fa1504451b643
nixps <lyderic@lyderic.local>
2017-06-20 15:02:24 +0200

    Added more tests for downloading apps.  Improved download code.

--- test/test.js
+++ test/test.js
@@ -11,13 +11,19 @@
 
 const mock = require('mock-require');
 const nock = require('nock');
+const fs = require('fs');
 const { assert } = require('chai');
+const mkdirp = require('mkdirp');
+const remove = require('remove');
+const streamify = require('stream-array');
+const os = require('os');
 
 class APIMockDelegate {
     constructor() {
         this._createSessionRequest = null;
         this._uploadedWhitepapers = [];
         this._deletedWhitepapers = [];
+        this._downloadedWhitepapers = [];
         this._deletedFiles = [];
         this._host = 'http://localhost:9090';
     }
@@ -69,6 +75,14 @@ class APIMockDelegate {
     fileDeleted(file) {
         this._deletedFiles.push(file);
     }
+
+    get downloadedWhitepapers() {
+        return this._downloadedWhitepapers;
+    }
+
+    whitepaperDownloaded(whitepaper) {
+        this._downloadedWhitepapers.push(whitepaper);
+    }
 }
 
 class APIMock {
@@ -110,6 +124,13 @@ class APIMock {
 
                 delete: function(whitepaper) {
                     mockDelegate.whitepaperDeleted(whitepaper);
+                },
+
+                download: function(whitepaper) {
+                    mockDelegate.whitepaperDownloaded(whitepaper);
+                    return {
+                        contents: 'contents'
+                    };
                 }
             },
 
@@ -122,9 +143,9 @@ class APIMock {
             },
 
             folder: {
-                list: function() {
+                list: function(query) {
                     return {
-                        results: mockDelegate.existingFolders()
+                        results: mockDelegate.existingFolders(query)
                     };
                 }
             }
@@ -147,6 +168,30 @@ function getFileUploadMock(uploadedFiles, expected) {
             }
         });
 }
+
+
+function getFileDownloadMock(downloadedFiles, expected) {
+    const downloadFileURLRegex = /portal.cgi\?dl=(.*)&session=session_admin_admin/;
+
+    nock('http://localhost:9090')
+        .get(downloadFileURLRegex, function() {
+            return true;
+        })
+        .times(expected)
+        .reply(200, function(uri) {
+            const matches = uri.match(downloadFileURLRegex);
+            if (Array.isArray(matches) && matches.length > 1) {
+                downloadedFiles.push(matches[1]);
+            }
+
+            return streamify(['1', '2', '3', os.EOF]);
+        }, {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="file.pdf"'
+        });
+}
+
+
 
 const apiMock = new APIMock();
 mock('cloudflow-api', apiMock);
@@ -208,6 +253,8 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp/images//win.png',
                             'cloudflow://PP_FILE_STORE/DemoApp/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
 
@@ -227,6 +274,8 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp/images//win.png',
                             'cloudflow://PP_FILE_STORE/DemoApp/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
 
@@ -245,6 +294,8 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp/images//linux.jpg',
                             'cloudflow://PP_FILE_STORE/DemoApp/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
 
@@ -272,6 +323,8 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp/images//win.png',
                             'cloudflow://PP_FILE_STORE/DemoApp/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
 
@@ -296,6 +349,8 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp/images//win.png',
                             'cloudflow://PP_FILE_STORE/DemoApp/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
             });
@@ -330,9 +385,210 @@ describe('cfapp tests', function() {
                             'cloudflow://PP_FILE_STORE/DemoApp3/images//win.png',
                             'cloudflow://PP_FILE_STORE/DemoApp3/index.html'
                         ], 'the files were not all uploaded');
+
+                        assert(nock.isDone(), 'expected requests not performed');
                     });
                 });
             });
+        });
+
+        describe('download', function() {
+            class ExistingSingleAppDelegate extends APIMockDelegate {
+                existingAssets(query) {
+                    if (query[2] === 'cloudflow://PP_FILE_STORE/DownloadApp/images//win.png') {
+                        return [{
+                            _id: 'I exist',
+                            cloudflow: {
+                                file: 'cloudflow://PP_FILE_STORE/DownloadApp/images/win.png'
+                            }
+                        }];
+                    }
+                    else if (query[2] === 'cloudflow://PP_FILE_STORE/DownloadApp/images//mac.png') {
+                        return [{
+                            _id: 'I exist too',
+                            cloudflow: {
+                                file: 'cloudflow://PP_FILE_STORE/DownloadApp/images/mac.png'
+                            }
+                        }];
+                    } else if (query[2] === 'cloudflow://PP_FILE_STORE/DownloadApp/images/') {
+                        // Get the assets in the image folder
+                        return [{
+                            _id: 'I exist',
+                            cloudflow: {
+                                file: 'cloudflow://PP_FILE_STORE/DownloadApp/images/win.png'
+                            }
+                        }, {
+                            _id: 'I exist too',
+                            cloudflow: {
+                                file: 'cloudflow://PP_FILE_STORE/DownloadApp/images/mac.png'
+                            }
+                        }];
+                    }
+
+                    return [];
+                }
+
+                get existingWhitepapers() {
+                    return [ {
+                        _id: 'Workflow1',
+                        name: 'Workflow1'
+                    }, {
+                        _id: 'Workflow2',
+                        name: 'Workflow2'
+                    }];
+                }
+
+                existingFolders(query) {
+                    if (query[2] === 'cloudflow://PP_FILE_STORE/DownloadApp/images/') {
+                        return [ 'images' ];
+                    }
+
+                    return [];
+                }
+            }
+
+            const projectCFApp = {
+                name: 'DownloadApp',
+                host: 'http://localhost:9090',
+                login: 'admin',
+                password: 'admin',
+                description: 'A test for downloading an application',
+
+                files: [
+                    'cloudflow://PP_FILE_STORE/DownloadApp/images/',
+                    'cloudflow://PP_FILE_STORE/DownloadApp/index.html',
+                ],
+
+                workflows: [
+                    'Workflow1',
+                    'Workflow2'
+                ]
+            };
+
+            after(function() {
+                if (fs.existsSync('./downloadTest')) {
+                    remove.removeSync('./downloadTest');
+                }
+            });
+
+            it('download a single application', function() {
+                apiMock.mockDelegate = new ExistingSingleAppDelegate();
+
+                const downloadedFiles = [];
+                getFileDownloadMock(downloadedFiles, 3);
+
+                if (fs.existsSync('./downloadTest') === true) {
+                    remove.removeSync('./downloadTest');
+                }
+                mkdirp.sync('./downloadTest/DownloadApp');
+                fs.writeFileSync('./downloadTest/DownloadApp/project.cfapp', JSON.stringify(projectCFApp));
+
+                return cfapp.apps.download('./downloadTest/DownloadApp').then(function() {
+                    const mockDelegate = apiMock.mockDelegate;
+                    assert.equal(mockDelegate.downloadedWhitepapers.length, 2, 'not all whitepapers were downloaded');
+                    assert.includeMembers(mockDelegate.downloadedWhitepapers, [
+                        'Workflow1',
+                        'Workflow2'
+                    ], 'not all whitepapers were downloaded');
+                    assert.equal(downloadedFiles.length, 3, 'not all files were downloaded');
+                    assert.includeMembers(downloadedFiles, [
+                        'cloudflow://PP_FILE_STORE/DownloadApp/images/win.png',
+                        'cloudflow://PP_FILE_STORE/DownloadApp/images/mac.png',
+                        'cloudflow://PP_FILE_STORE/DownloadApp/index.html'
+                    ], 'not all files were downloaded');
+                    assert(nock.isDone(), 'expected requests not performed');
+
+                    // TODO: assert file structure too
+                });
+            });
+
+            it('should not overwrite', function() {
+                apiMock.mockDelegate = new ExistingSingleAppDelegate();
+
+                const downloadedFiles = [];
+                getFileDownloadMock(downloadedFiles, 3);
+
+                const projectCFApp = {
+                    name: 'DownloadApp',
+                    host: 'http://localhost:9090',
+                    login: 'admin',
+                    password: 'admin',
+                    description: 'A test for downloading an application',
+
+                    files: [
+                        'cloudflow://PP_FILE_STORE/DownloadApp/images/',
+                        'cloudflow://PP_FILE_STORE/DownloadApp/index.html',
+                    ],
+
+                    workflows: [
+                        'Workflow1',
+                        'Workflow2'
+                    ]
+                };
+
+                if (fs.existsSync('./downloadTest') === true) {
+                    remove.removeSync('./downloadTest');
+                }
+                mkdirp.sync('./downloadTest/DownloadApp');
+                fs.writeFileSync('./downloadTest/DownloadApp/project.cfapp', JSON.stringify(projectCFApp));
+
+                return cfapp.apps.download('./downloadTest/DownloadApp').then(function() {
+                    // Reset the mock delegate
+                    nock.cleanAll();
+                    apiMock.mockDelegate = new ExistingSingleAppDelegate();
+                    const downloadedFiles = [];
+                    getFileDownloadMock(downloadedFiles, 1);
+
+                    return cfapp.apps.download('./downloadTest/DownloadApp').then(function() {
+                        const mockDelegate = apiMock.mockDelegate;
+                        assert.equal(mockDelegate.downloadedWhitepapers.length, 0, 'no whitepapers should be downloaded');
+                        assert.equal(downloadedFiles.length, 0, 'no files should be downloaded');
+                        assert(nock.isDone() === false, 'requests were performed while none expected');
+                        // TODO: assert file structure too
+                    });
+                });
+            });
+
+
+            it('should overwrite when the flag is set', function() {
+                apiMock.mockDelegate = new ExistingSingleAppDelegate();
+
+                const downloadedFiles = [];
+                getFileDownloadMock(downloadedFiles, 3);
+
+                if (fs.existsSync('./downloadTest') === true) {
+                    remove.removeSync('./downloadTest');
+                }
+                mkdirp.sync('./downloadTest/DownloadApp');
+                fs.writeFileSync('./downloadTest/DownloadApp/project.cfapp', JSON.stringify(projectCFApp));
+
+                return cfapp.apps.download('./downloadTest/DownloadApp').then(function() {
+                    // Reset the mock delegate
+                    nock.cleanAll();
+                    apiMock.mockDelegate = new ExistingSingleAppDelegate();
+                    const downloadedFiles = [];
+                    getFileDownloadMock(downloadedFiles, 3);
+
+                    return cfapp.apps.download('./downloadTest/DownloadApp', {
+                        overwrite: true
+                    }).then(function() {
+                        const mockDelegate = apiMock.mockDelegate;
+                        assert.equal(mockDelegate.downloadedWhitepapers.length, 2, 'not all whitepapers were downloaded');
+                        assert.includeMembers(mockDelegate.downloadedWhitepapers, [
+                            'Workflow1',
+                            'Workflow2'
+                        ], 'not all whitepapers were downloaded');
+                        assert.equal(downloadedFiles.length, 3, 'not all files were downloaded');
+                        assert.includeMembers(downloadedFiles, [
+                            'cloudflow://PP_FILE_STORE/DownloadApp/images/win.png',
+                            'cloudflow://PP_FILE_STORE/DownloadApp/images/mac.png',
+                            'cloudflow://PP_FILE_STORE/DownloadApp/index.html'
+                        ], 'not all files were downloaded');
+                        assert(nock.isDone(), 'expected requests not performed');
+                    });
+                });
+            });
+
         });
     });
 });
