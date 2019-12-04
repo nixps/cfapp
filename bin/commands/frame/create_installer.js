797d9739bb07e442de503ba51ed8fda83346fd26
Lennart Tondeleir <lennartt@hybridsoftware.com>
2019-12-04 13:39:51 +0100

    fix for cep extensions on win + optional sign for mac

--- bin/commands/frame/create_installer.js
+++ bin/commands/frame/create_installer.js
@@ -15,6 +15,19 @@ var frame=require("../../../lib/libframe.js");
 module.exports = {
     command: 'create_installer <software_folder> <output_location>',
     desc: 'Creates a .pkg installer',
-    builder: {},
-    handler: frame.create_installer
+    builder: function (yargs) {
+        yargs.example('$0 frame create_installer', 'creates an installer without signing')
+            .example('$0 frame create_installer --sign', 'creates an installer with signing')
+            .option('sign', {
+                describe: 'signs the installer',
+                default: false
+            })
+    },
+    handler: function (argv) {
+        if (argv.sign) {
+            frame.create_installer(argv);
+        } else {
+            frame.create_installer_nosign(argv);
+        }
+    }
 };
