fe61c5c5b0a40ca19fd2d6b1c0a4e4eed2bb16f6
Lennart Tondeleir <lennartt@hybridsoftware.com>
2020-03-13 13:49:54 +0100

    #32309: notarization

---
+++ bin/commands/frame/notarize.js
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2017 NiXPS, All rights reserved.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+
+'use strict';
+
+var frame=require("../../../lib/libframe.js");
+
+module.exports = {
+    command: 'notarize <pkg_installer>',
+    desc: 'Notarizes and staples a .pkg installer',
+    builder: {},
+    handler: frame.notarize
+    }
