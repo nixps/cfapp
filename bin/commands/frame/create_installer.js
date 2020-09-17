97e7b4feafc651df27f61199ee197cecb363ff5d
Lennart Tondeleir <lennartt@hybridsoftware.com>
2020-09-17 13:52:06 +0200

    Revert "#32133: notarize plug-in suite"
    This reverts commit 93d2722c37fe6db888d595aae6d8146f38756193.

--- bin/commands/frame/create_installer.js
+++ bin/commands/frame/create_installer.js
@@ -16,18 +16,18 @@ module.exports = {
     command: 'create_installer <software_folder> <output_location>',
     desc: 'Creates a .pkg installer',
     builder: function (yargs) {
-        yargs.example('$0 frame create_installer', 'creates an installer')
-            .example('$0 frame create_installer --nosign', 'creates an installer without signing')
-            .option('nosign', {
-                describe: 'skips the code signsing of the installer',
+        yargs.example('$0 frame create_installer', 'creates an installer without signing')
+            .example('$0 frame create_installer --sign', 'creates an installer with signing')
+            .option('sign', {
+                describe: 'signs the installer',
                 default: false
             })
     },
     handler: function (argv) {
-        if (argv.nosign) {
-            frame.create_installer_nosign(argv);
-        } else {
+        if (argv.sign) {
             frame.create_installer(argv);
+        } else {
+            frame.create_installer_nosign(argv);
         }
     }
 };
